16546677bb6255fece94d7ab192e87c55687d927
Olivia Wilson <olivia@rtwilson.com>
2015-10-18 22:32:20 +0100

    Added code to remove padding from convolved. Test suite working for convolve.
    Test suite now checks that my convolve and the scipy convolve are the same to 6 decimal places, whether the size of out is the same as input and if errors are raised when even kernels are given.
    
    In hybrid_image.py added function to check more arbitrary odd numbers, and supported types. Padding is removed from the array convolved to return image without borders. The gaussian kernel creator has odd and type checking and has the beginnings of workings.

--- hybrid_image.py
+++ hybrid_image.py
@@ -9,21 +9,47 @@ from numpy.testing import assert_array_equal
 # Seperate the high pass and low pass and then put into the hybrid function
 # This creates an easy way of showing each step of the process.
 
-def convolve(image, kernel):
-    # Check that the kernal is an odd size
-    if kernel.size%2 == 0:
-        raise TypeError("Only kernels of odd size are supported. "
-                        "Got {shape}.".format(shape = kernel.shape))
+def _odd(number):
+    """Raises an error if a number is odd"""
+    if number % 2 == 0:
+        raise TypeError("Only odd sizes are supported. "
+                        "Got {number}.".format(number = number))
 
-    #Creates variable for later use to return original sized image
+def _check_type_supported(n): 
+    if not (np.issubdtype(type(n), np.integer)):
+        raise TypeError("Only integer types are supported. "
+                        "Got %s." % type(n))
+
+def convolve(image, kernel):
+    """Convolves a kernel over an image using the fast fourier transform.
+
+    Parameters
+    ----------
+    image: ndarray (arbitrary shape,float or int type)
+        Image to be convolved over.
+    kernel: ndarray
+        Kernel to convolve with of odd shape not necessarily square.
+
+    Raises
+    ------
+    TypeError
+        If the input kernel is of even shape in one or both dimensions.
+
+    Returns
+    -------
+    out: ndarray, same shape as input `image`
+        The convolved image
+    """
+
+    # Check kernel is odd
+    _odd(kernel.size)
+
+    # Creates variable for later use to return original sized image
     image_size = image.shape
 
     # Creates variable of how many rows with which the image needs to be padded
     pad_shape = np.array(image.shape) + int(np.floor(len(kernel)/2))*2
     pad_shape = list(pad_shape)
-
-    # Calls numpy.pad to create border of zeros half the size of the kernel
-    # padded = np.pad(image, pad_width, mode = 'constant')
 
     # Calculate FFT for image: using the option to pad with zeros to the 
     # the necessary size 
@@ -40,21 +66,57 @@ def convolve(image, kernel):
     # to the original image size.
     convolved = np.fft.ifftn(fft_convolved)
 
-    # Write something to take it back to origin shape using image_size
+    # Calculate how much padding has been added by the fft
+    expanded = np.array(convolved.shape) - np.array(image_size)
 
-    return np.real(convolved)
+    # Padding by the fft is always equal on each side so taking half of the 
+    # difference provides padding on each side.
+    pad = int((expanded/2)[0])
+
+    # Index the new array to get the central array without the padding
+    out = convolved[pad:-pad, pad:-pad]
+
+    # Take the real part of the convolved array
+    out = np.real(out)
+
+    return out
 
 
 def create_gaussian_kernel(n):
-    """Creates Gaussian kernel of size n to do smoothing.
+    """Creates Gaussian kernel of size n and standard deviation sigma.
 
     Parameters
     ----------
-    n: string
-        The type of kernel to be created. Choices include:
-        "gaussian",
-        "average",
+    n: int
+        The size of kernel to be created it will return an n x n array.
+    sigma: int or float
+        The standard deviation of the resulting gaussian kernel
+
+    Raises
+    ------
+    TypeError
+        If the input n is odd.
+
+    Returns
+    -------
+    kernel: ndarray, of size n x n
+        A kernel 
     """
+
+    # Check that n is of type int
+    _check_type_supported(n)
+
+    # Check that n is an odd size
+    _odd(n)
+
+    # Find centre of window
+    centre = floor(n/2) + 1
+
+    # Normalise by the total sum
+    total_sum = 0
+
+    #
+
     return kernel
 
 
--- test_hybrid_images.py
+++ test_hybrid_images.py
@@ -1,18 +1,54 @@
 import numpy as np
 from numpy.testing import (assert_array_equal, assert_equal, assert_raises,
-                           assert_warns)
-from skimage.morphology import remove_small_objects, remove_small_holes
-from ..._shared._warnings import expected_warnings
+                           assert_array_almost_equal)
+from hybrid_image import convolve
+import scipy.ndimage.filters as filt
 
-# Create test image here!
-test_image = 
+
+test_image = np.array([[ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+                       [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+                       [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+                       [ 0.,  0.,  0.,  1.,  1.,  1.,  1.,  0.,  0.,  0.],
+                       [ 0.,  0.,  0.,  1.,  1.,  1.,  1.,  0.,  0.,  0.],
+                       [ 0.,  0.,  0.,  1.,  1.,  1.,  1.,  0.,  0.,  0.],
+                       [ 0.,  0.,  0.,  1.,  1.,  1.,  1.,  0.,  0.,  0.],
+                       [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+                       [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
+                       [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.]])
 
 even_kernel = np.array([[0, 0, 0, 0],
                         [1, 1, 1, 0],
                         [1, 1, 1, 1]], bool)
 
+kernel = np.array([[ 0.1111,  0.1111,  0.1111],
+                   [ 0.1111,  0.1111,  0.1111],
+                   [ 0.1111,  0.1111,  0.1111]])
+
 def test_convolve_even_kernel_error():
-    assert_raises(TypeError,convolve,even_kernel,test_image)
+    """ Tests whether errors are thrown when given an even kernel"""
+
+    assert_raises(TypeError, convolve, test_image, even_kernel)
+
+def test_convolve_vs_scipy():
+    """Tests the output of my convolve v.s. scipy.ndimage.filters.convolve"""
+
+    scipy_conv = filt.convolve(test_image, kernel, mode = 'constant')
+
+    conv_arr = convolve(test_image, kernel)
+
+    # Checks that the array is equal within 6 decimal places due to small rounding errors
+    assert_array_almost_equal(scipy_conv, conv_arr)
+
+def test_out_size_equal():
+    """Tests that the size of the output array is equal to the size of the input array"""
+
+    expected = test_image.shape
+
+    conv_arr = convolve(test_image, kernel)
+
+    assert_equal(actual = conv_arr.shape, desired = expected)
+
+
 
 # if __name__ == "__main__":
 #     np.testing.run_module_suite()
